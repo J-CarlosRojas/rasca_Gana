abd84c20353cf1e3bfea2e33358796f9a0892041
J Carlos Rojas <j.carlosrojas@outlook.com>
2023-12-13 23:18:47 -0500

    funcion comprar boletos

--- script/script.js
+++ script/script.js
@@ -3,11 +3,32 @@ document.addEventListener("DOMContentLoaded", function () {
     const juego = {
         dinero: 0.00,
         tasaGanancia: 0.01,
+        boletosComprados: 0,
+        precioBoletoActual: 0,
+
+
         montoElement: document.getElementById("monto"),
         boletos: Array.from({ length: 6 }, (_, i) => ({ id: i + 1, ganancia: 0 })),
         actualizarContador: function () {
             juego.dinero += juego.tasaGanancia;
             juego.montoElement.textContent = juego.dinero.toFixed(2);
+        },
+        comprarBoleto: function (precio) {
+            // Verifica si el usuario tiene suficiente dinero para comprar el boleto
+            if (juego.dinero >= precio) {
+                // Deduce el precio del boleto del dinero del usuario
+                juego.dinero -= precio;
+                juego.montoElement.textContent = juego.dinero.toFixed(2);
+
+                // Incrementa el número de boletos comprados y actualiza el precio del próximo boleto
+                juego.boletosComprados++;
+                juego.precioBoletoActual = juego.calcularPrecioSiguienteBoleto();
+
+                // Muestra un mensaje al usuario
+                alert(`¡Boleto comprado por $${precio.toFixed(2)}!\nNúmero de boletos comprados: ${juego.boletosComprados}`);
+            } else {
+                alert("No tienes suficiente dinero para comprar este boleto.");
+            }
         },
         revelarGanancia: function (boletoId) {
             const boleto = juego.boletos.find(b => b.id === boletoId);
@@ -39,6 +60,10 @@ document.addEventListener("DOMContentLoaded", function () {
             // Actualiza el montoElement con el nuevo valor de dinero
             juego.montoElement.textContent = juego.dinero.toFixed(2);
         },
+        calcularPrecioSiguienteBoleto: function () {
+            // Precio base más un incremento por boleto comprado
+            return 0.60 + (juego.boletosComprados * 0.20); // Incremento de $0.20 por boleto
+        },
         iniciarJuego: function () {
             // Iniciar el intervalo para la actualización del contador
             const intervalo = setInterval(juego.actualizarContador, 1000); // Actualizar cada segundo
@@ -48,6 +73,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 clearInterval(intervalo);
             }, 60000);
         }
+        
     };
 
     // Asignar la variable juego al objeto window para que sea accesible desde otros ámbitos
